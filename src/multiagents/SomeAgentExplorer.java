70bde8dee08b0cb1ea7338f4f535ad11b9fc0139
Cristopherson Torres <cristopherson.torres@gmail.com>
2015-05-04 22:46:44 -0500

    Add base explorer class  behavior

--- src/multiagents/SomeAgentExplorer.java
+++ src/multiagents/SomeAgentExplorer.java
@@ -6,10 +6,13 @@
 package multiagents;
 
 import apltk.interpreter.data.LogicBelief;
+import apltk.interpreter.data.LogicGoal;
 import apltk.interpreter.data.Message;
 import eis.iilang.Action;
 import eis.iilang.Percept;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.Vector;
 import massim.javaagents.agents.MarsUtil;
 import massim.javaagents.agents.SimpleExplorerAgent;
@@ -30,7 +33,40 @@ public class SomeAgentExplorer extends massim.javaagents.Agent {
         //deliberate and return an action
         handleMessages();
         handlePercepts();
-        return null;
+
+        Action act = null;
+
+        // 1. recharging
+        act = planRecharge();
+        if (act != null) {
+            return act;
+        }
+
+        // 2. buying battery with a certain probability
+        act = planBuyBattery();
+        if (act != null) {
+            return act;
+        }
+
+        // 3. probing if necessary
+        act = planProbe();
+        if (act != null) {
+            return act;
+        }
+
+        // 4. surveying if necessary
+        act = planSurvey();
+        if (act != null) {
+            return act;
+        }
+
+        // 5. (almost) random walking
+        act = planRandomWalk();
+        if (act != null) {
+            return act;
+        }
+
+        return MarsUtil.skipAction();
     }
 
     @Override
@@ -43,6 +79,7 @@ public class SomeAgentExplorer extends massim.javaagents.Agent {
         // handle messages... believe everything the others say
         Collection<Message> messages = getMessages();
         for (Message msg : messages) {
+
             println(msg.sender + " told me " + msg.value);
             String predicate = ((LogicBelief) msg.value).getPredicate();
             if (containsBelief((LogicBelief) msg.value)) {
@@ -147,4 +184,214 @@ public class SomeAgentExplorer extends massim.javaagents.Agent {
         }
     }
 
+    private Action planRecharge() {
+
+        LinkedList<LogicBelief> beliefs = null;
+
+        beliefs = getAllBeliefs("energy");
+        if (beliefs.size() == 0) {
+            println("strangely I do not know my energy");
+            return MarsUtil.skipAction();
+        }
+        int energy = new Integer(beliefs.getFirst().getParameters().firstElement()).intValue();
+
+        beliefs = getAllBeliefs("maxEnergy");
+        if (beliefs.size() == 0) {
+            println("strangely I do not know my maxEnergy");
+            return MarsUtil.skipAction();
+        }
+        int maxEnergy = new Integer(beliefs.getFirst().getParameters().firstElement()).intValue();
+
+        // if has the goal of being recharged...
+        if (goals.contains(new LogicGoal("beAtFullCharge"))) {
+            if (maxEnergy == energy) {
+                println("I can stop recharging. I am at full charge");
+                removeGoals("beAtFullCharge");
+            } else {
+                println("recharging...");
+                return MarsUtil.rechargeAction();
+            }
+        } // go to recharge mode if necessary
+        else {
+            if (energy < maxEnergy / 3) {
+                println("I need to recharge");
+                goals.add(new LogicGoal("beAtFullCharge"));
+                return MarsUtil.rechargeAction();
+            }
+        }
+
+        return null;
+
+    }
+
+    private Action planProbe() {
+
+        LinkedList<LogicBelief> beliefs = null;
+
+        beliefs = getAllBeliefs("position");
+        if (beliefs.size() == 0) {
+            println("strangely I do not know my position");
+            return MarsUtil.skipAction();
+        }
+        String position = beliefs.getFirst().getParameters().firstElement();
+
+        // probe current position if not known
+        boolean probed = false;
+        LinkedList<LogicBelief> vertices = getAllBeliefs("probedVertex");
+        for (LogicBelief v : vertices) {
+            if (v.getParameters().get(0).equals(position)) {
+                probed = true;
+                break;
+            }
+        }
+        if (probed == false) {
+            println("I do not know the value of my position. I will probe.");
+            return MarsUtil.probeAction();
+        } else {
+            println("I know the value of my position");
+        }
+
+        beliefs = getAllBeliefs("neighbor");
+
+        // get unprobed neighbors
+        Vector<String> unprobed = new Vector<String>();
+        for (LogicBelief n : beliefs) {
+            probed = false;
+            String name = n.getParameters().firstElement();
+            for (LogicBelief v : vertices) {
+                if (v.getParameters().get(0).equals(name)) {
+                    probed = true;
+                    break;
+                }
+            }
+            if (probed == false) {
+                unprobed.add(name);
+            }
+        }
+        if (unprobed.size() != 0) {
+            println("some of my neighbors are unprobed.");
+            Collections.shuffle(unprobed);
+            String neighbor = unprobed.firstElement();
+            println("I will go to " + neighbor);
+            return MarsUtil.gotoAction(neighbor);
+        } else {
+            println("all of my neighbors are probed");
+        }
+
+        return null;
+
+    }
+
+    private Action planSurvey() {
+
+        println("I know " + getAllBeliefs("visibleEdge").size() + " visible edges");
+        println("I know " + getAllBeliefs("surveyedEdge").size() + " surveyed edges");
+
+        // get all neighbors
+        LinkedList<LogicBelief> visible = getAllBeliefs("visibleEdge");
+        LinkedList<LogicBelief> surveyed = getAllBeliefs("surveyedEdge");
+
+        String position = getAllBeliefs("position").get(0).getParameters().firstElement();
+
+        int unsurveyedNum = 0;
+        int adjacentNum = 0;
+
+        for (LogicBelief v : visible) {
+
+            String vVertex0 = v.getParameters().elementAt(0);
+            String vVertex1 = v.getParameters().elementAt(1);
+
+            boolean adjacent = false;
+            if (vVertex0.equals(position) || vVertex1.equals(position)) {
+                adjacent = true;
+            }
+
+            if (adjacent == false) {
+                continue;
+            }
+            adjacentNum++;
+
+            boolean isSurveyed = false;
+            for (LogicBelief s : surveyed) {
+                String sVertex0 = s.getParameters().elementAt(0);
+                String sVertex1 = s.getParameters().elementAt(1);
+                if (sVertex0.equals(vVertex0) && sVertex1.equals(vVertex1)) {
+                    isSurveyed = true;
+                    break;
+                }
+                if (sVertex0.equals(vVertex1) && sVertex1.equals(vVertex0)) {
+                    isSurveyed = true;
+                    break;
+                }
+            }
+            if (isSurveyed == false) {
+                unsurveyedNum++;
+            }
+
+        }
+
+        println("" + unsurveyedNum + " out of " + adjacentNum + " adjacent edges are unsurveyed");
+
+        if (unsurveyedNum > 0) {
+            println("I will survey");
+            return MarsUtil.surveyAction();
+        }
+
+        return null;
+
+    }
+
+    /**
+     * Buy a battery with a given probability
+     *
+     * @return
+     */
+    private Action planBuyBattery() {
+
+        LinkedList<LogicBelief> beliefs = this.getAllBeliefs("money");
+        if (beliefs.size() == 0) {
+            println("strangely I do not know our money.");
+            return null;
+        }
+
+        LogicBelief moneyBelief = beliefs.get(0);
+        int money = new Integer(moneyBelief.getParameters().get(0)).intValue();
+
+        if (money < 10) {
+            println("we do not have enough money.");
+            return null;
+        }
+        println("we do have enough money.");
+
+        //double r = Math.random();
+        //if ( r < 1.0 ) {
+        //	println("I am not going to buy a battery");
+        //	return null;
+        //}
+        println("I am going to buy a battery");
+
+        return MarsUtil.buyAction("battery");
+
+    }
+
+    private Action planRandomWalk() {
+
+        LinkedList<LogicBelief> beliefs = getAllBeliefs("neighbor");
+        Vector<String> neighbors = new Vector<String>();
+        for (LogicBelief b : beliefs) {
+            neighbors.add(b.getParameters().firstElement());
+        }
+
+        if (neighbors.size() == 0) {
+            println("strangely I do not know any neighbors");
+            return MarsUtil.skipAction();
+        }
+
+        // goto neighbors
+        Collections.shuffle(neighbors);
+        String neighbor = neighbors.firstElement();
+        println("I will go to " + neighbor);
+        return MarsUtil.gotoAction(neighbor);
+
+    }
 }
